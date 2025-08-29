15f1c775fbe2e444a7e017d500f485a6ecfcdd3e
Stanislav Georgiev <2494186@cstjean.qc.ca>
2025-08-29 20:57:25 +0000

    init

---
+++ .bashrc
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "Bonjour etudiant!"
+    alias ls='ls -a'
+    alias ll='ls -a -l'
+    alias c='clear'
+    alias grepc='grep --color=auto'
+
+    VimOpen(){
+        nvim fichier.c
+    }
+
+    search(){
+        grep -i "texte"
+    }
---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+mkdir bin
+mkdir cegep
+mkdir -p cegep/projet
+
+sudo pacman -S git
+sudo pacman -S neovim
+sudo pacman -S base-devel
+sudo pacman -S gbd
+sudo pacman -S python
+sudo pacman -S python-pip
+
+
+echo cp dotfile
+
+move()
+{
+    mkdir .config
+    cp .bashrc ~/.config
+}
